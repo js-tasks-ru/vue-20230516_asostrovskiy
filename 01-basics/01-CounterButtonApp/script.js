765723c7b993aa6fc7b785e3ca791ff8f3f28f3b
VUIVR <asostrovskiy@yandex.ru>
2023-05-17 15:27:53 +0400

    done 01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,23 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = createApp({
+	data() {
+		return {
+			count: 0
+		}
+	},
+	methods: {
+		increment (){
+			this.count += 1
+		},
+		reset (){
+			this.count = 0
+		}
+	},
+	template: `<button type='button' @click='increment'> {{ count }} </button> <br/>
+	 <button type='button' @click='reset'>Reset</button>`
+
+})
+
+App.mount('#app')
+
