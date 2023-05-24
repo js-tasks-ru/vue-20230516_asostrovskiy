c06414b90e6e6f9a06b345a7c51ba7f94a87530d
VUIVR <asostrovskiy@yandex.ru>
2023-05-24 13:48:45 +0400

    task 02/06

---
+++ 02-components/06-MeetupView/components/MeetupAgenda.js
@@ -0,0 +1,25 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+
+export default defineComponent({
+  name: 'MeetupAgenda',
+
+  components: {
+	MeetupAgendaItem
+	},
+
+  props: {
+	agenda: {
+		type: Array,
+		required: true
+	}
+  },
+
+  template: `
+    <ul class="agenda">
+      <li v-for="item in agenda" :key='item.id' class="agenda__item">
+        <MeetupAgendaItem :agendaItem='item' />
+      </li>
+	  <UiAlert> Программа пока пуста... </UiAlert>
+    </ul>`,
+});
--- 02-components/06-MeetupView/components/MeetupView.js
+++ 02-components/06-MeetupView/components/MeetupView.js
@@ -1,13 +1,22 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-import UiAlert from './UiAlert.js';
+import MeetupCover from "./MeetupCover.js";
 import UiContainer from './UiContainer.js';
+import MeetupDescription from "./MeetupDescription.js";
+import MeetupAgenda from "./MeetupAgenda.js";
+import UiAlert from './UiAlert.js';
+import MeetupInfo from "./MeetupInfo.js";
+
 
 export default defineComponent({
   name: 'MeetupView',
 
   components: {
-    UiAlert,
+	MeetupCover,
     UiContainer,
+	MeetupDescription,
+	MeetupAgenda,
+    UiAlert,
+	MeetupInfo,
   },
 
   props: {
@@ -19,22 +28,28 @@ export default defineComponent({
 
   template: `
     <div>
-      <!-- meetup cover-->
+        <MeetupCover :title="meetup.title" :image="meetup.image" />
 
-      <UiContainer>
-        <div class="meetup">
-          <div class="meetup__content">
-            <h3>Описание</h3>
-            <!-- meetup description -->
+		<UiContainer>
+			<div class="meetup">
+			<div class="meetup__content">
+				<h3>Описание</h3>
+				<MeetupDescription :description="meetup.description" />
 
-            <h3>Программа</h3>
-            <!-- meetup agenda -->
-            <UiAlert>Программа пока пуста...</UiAlert>
-          </div>
-          <div class="meetup__aside">
-            <!-- meetup info -->
-          </div>
-        </div>
-      </UiContainer>
+				<h3>Программа</h3>
+				<template v-if="meetup.agenda.length">
+					<MeetupAgenda :agenda="meetup.agenda" />
+				</template>
+				<UiAlert v-else> Программа пока пуста... </UiAlert>
+			</div>
+			<div class="meetup__aside">
+				<MeetupInfo
+					:organizer="meetup.organizer"
+  					:place="meetup.place"
+  					:date="meetup.date"
+					/>
+			</div>
+			</div>
+		</UiContainer>
     </div>`,
 });
